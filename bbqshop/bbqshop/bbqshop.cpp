472680a1b6518eccb3524afeeeb0f77bb5b7ab0c
qinan <478396100@qq.com>
2017-07-20 10:44:27 +0800

    主进程隐藏操作

--- bbqshop/bbqshop/bbqshop.cpp
+++ bbqshop/bbqshop/bbqshop.cpp
@@ -1,8 +1,17 @@
-#include "bbqshop.h"
+﻿#include "bbqshop.h"
+#include "AllWindowTitle.h"
+#include <QTimer>
+#include "zhfunclib.h"
 
 bbqshop::bbqshop(QWidget *parent)
 	: QWidget(parent)
 {
+	setWindowFlags(Qt::FramelessWindowHint|Qt::WindowStaysOnTopHint|Qt::Popup|Qt::Tool);
+	setWindowTitle(FLOATWINTITLE);
+	setGeometry(0, 0, 5, 5);
+	// 定时器
+	QTimer::singleShot(100,this, SLOT(hide()) );  // 隐藏自己
+
 }
 
 bbqshop::~bbqshop()
--- bbqshop/bbqshop/main.cpp
+++ bbqshop/bbqshop/main.cpp
@@ -1,11 +1,15 @@
-#include "bbqshop.h"
+﻿#include "bbqshop.h"
 #include <QtWidgets/QApplication>
 #include "zhfunclib.h"
 #include "AllExeName.h"
 
 int main(int argc, char *argv[])
 {
+	// 关闭除本进程外的其他进程
+	ZHFuncLib::TerminateProcessExceptCurrentOne(BBQSHOPEXE);
+
 	QApplication a(argc, argv);
+	a.setQuitOnLastWindowClosed(false);
 	bbqshop w;
 	w.show();
 	return a.exec();
