5ba6420eec2e2f0e132e7d4f243a6b6f615ae779
qinan@bboqi.com <478396100@qq.com>
2017-08-29 22:14:46 +0800

    Excel读取后再写入遇到问题了

--- bbqshop/ExceTest/ExcelTestDemo.cpp
+++ bbqshop/ExceTest/ExcelTestDemo.cpp
@@ -10,9 +10,12 @@ ExcelTestDemo::ExcelTestDemo(QString filePath, QObject *parent)
 	work_books->dynamicCall("Open (const QString&)", filePath);
 	//QVariant title_value = applicationExcel->property("Caption");  //获取标题
 	QAxObject *workBook = applicationExcel->querySubObject("ActiveWorkBook");
-	QAxObject *workSheets = workBook->querySubObject("WorkSheets");
+	//QAxObject *workSheets = workBook->querySubObject("WorkSheets");
 	//QAxObject *curSheet = workSheets->querySubObject("Item(const QString&)", "Sheet1");
-
+	QAxObject *worksheet = workBook->querySubObject("Worksheets(int)", 1);  
+	QAxObject *cell1 = worksheet->querySubObject("Cells(int,int)", 1, 1 ); 
+	QString val1 = cell1->property("Value").toString();//property是返回QVariant结构体
+	bool issuc = cell1 ->setProperty("Value", QString::fromLocal8Bit("时间2人员"));  //设置单元格值
 }
 
 ExcelTestDemo::~ExcelTestDemo()
